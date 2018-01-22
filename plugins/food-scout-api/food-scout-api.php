e876a567cdb1b05b2c511a96440d314ad00f8765
Marcus Battle <marcusbattle@marcbook.attlocal.net>
2018-01-22 04:55:00 -0500

    Added geolocation to the /food endpoint

--- plugins/food-scout-api/food-scout-api.php
+++ plugins/food-scout-api/food-scout-api.php
@@ -48,8 +48,11 @@ class Food_Scout_API {
 		add_action( 'init', [ $this, 'register_food_cpt' ], 10 );
 		add_action( 'init', [ $this, 'register_taste_taxonomy' ], 10 );
 
+		// Actions.
+		add_action( 'save_post', [ $this, 'set_restaurant_geolocation' ], 50, 3 );
+
 		// Relationships.
-		add_action( 'p2p_init', [ $this, 'register_food_p2p_connection'] );
+		add_action( 'p2p_init', [ $this, 'register_food_p2p_connection' ] );
 
 		// API.
 		add_action( 'rest_api_init', [ $this, 'register_api_endpoints' ] );
@@ -122,6 +125,61 @@ class Food_Scout_API {
 	}
 
 	/**
+	 * Get the restauratn geolocation from Google Maps API.
+	 *
+	 * If this doesn't work, the alternative is https://geocod.io/.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param integer $post_id     The post ID.
+	 * @param WP_Post $post_after  The post object after the update.
+	 * @param WP_Post $post_before The post object before the update.
+	 */
+	public function set_restaurant_geolocation( $post_id, $post_after, $post_before ) {
+
+		$restaurant_address = str_replace( ' ', '+', $this->build_restaurant_address( $post_id ) );
+		$geonames_url       = "http://maps.googleapis.com/maps/api/geocode/json?address={$restaurant_address}";
+
+		$response = wp_remote_get( $geonames_url, array(
+			'timeout' => 30,
+		) );
+
+		if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
+			return false;
+		}
+
+		$data = json_decode( wp_remote_retrieve_body( $response ) );
+
+		// Capture the geolocation.
+		$geolocation = isset( $data->results[0]->geometry->location ) ? $data->results[0]->geometry->location : null;
+
+		$latitude  = $geolocation->lat;
+		$longitude = $geolocation->lng;
+
+		update_post_meta( $post_id, 'latitude', $latitude );
+		update_post_meta( $post_id, 'longitude', $longitude );
+	}
+
+	/**
+	 * Build the restaurant address into one string.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param integer $post_id The post ID.
+	 *
+	 * @return string $restaurant_address
+	 */
+	public function build_restaurant_address( $post_id ) {
+
+		$restaurant_address  = get_post_meta( $post_id, 'address', true );
+		$restaurant_address .= ' ' . get_post_meta( $post_id, 'city', true );
+		$restaurant_address .= ', ' . get_post_meta( $post_id, 'state', true );
+		$restaurant_address .= ' ' . get_post_meta( $post_id, 'zip', true );
+
+		return $restaurant_address;
+	}
+
+	/**
 	 * Register the API endpoints.
 	 *
 	 * @since 0.1.0
@@ -172,21 +230,64 @@ class Food_Scout_API {
 	 */
 	public function get_food_via_api( $request ) {
 
-		$taste = $request->get_param( 'taste' );
+		$taste     = $request->get_param( 'taste' );
+		$latitude  = $request->get_param( 'latitude' );
+		$longitude = $request->get_param( 'longitude' );
+		$radius    = $request->get_param( 'radius' ) ? $request->get_param( 'radius' ) : 25;
 
-		$food_query = array(
-			'post_type'   => 'food',
-			'post_status' => 'publish',
-			'tax_query'   => array(
+		// Get the restaurants first. Then get all of the food from those restuarants.
+		global $wpdb;
+
+		// @TODO: Add LIMIT to query.
+		$sql = "
+		SELECT ID, distance 
+		FROM (
+			SELECT 
+				posts.ID,
+				3956 * ACOS( COS( RADIANS( {$latitude} ) ) * COS( RADIANS( latitude.meta_value ) ) * COS( RADIANS( {$longitude} ) - RADIANS( longitude.meta_value ) ) + SIN( RADIANS( {$latitude} ) ) * SIN( RADIANS( latitude.meta_value ) ) ) AS distance,
+				latitude.meta_value AS latitude, 
+				{$latitude} - ( {$radius} / 69 ) AS latitude_min,
+				{$latitude} + ( {$radius} / 69 ) AS latitude_max,
+				longitude.meta_value AS longitude,
+				{$longitude} - ( {$radius} / ( 69 * COS( RADIANS( {$latitude} ) ) ) ) AS longitude_min,
+				{$longitude} + ( {$radius} / ( 69 * COS( RADIANS( {$latitude} ) ) ) ) AS longitude_max
+			FROM $wpdb->posts AS posts
+			LEFT JOIN $wpdb->postmeta latitude ON ( posts.ID = latitude.post_id )
+			LEFT JOIN $wpdb->postmeta longitude ON ( posts.ID = longitude.post_id )
+			WHERE
+				latitude.meta_key = 'latitude' AND latitude.meta_value IS NOT NULL
+				AND longitude.meta_key = 'longitude' AND longitude.meta_value IS NOT NULL
+				AND latitude.meta_value BETWEEN ( {$latitude} - ( {$radius} / 69 ) ) AND ( {$latitude} + ( {$radius} / 69 ) )
+				AND longitude.meta_value BETWEEN ( {$longitude} - ( {$radius} / ( 69 * COS( RADIANS( {$latitude} ) ) ) ) ) AND ( {$longitude} + ( {$radius} / ( 69 * COS( RADIANS( {$latitude} ) ) ) ) )
+		) r
+		WHERE distance < {$radius}
+		ORDER BY distance ASC";
+
+		$results = $wpdb->get_results( $sql );
+
+		if ( empty( $results ) ) {
+			wp_send_json_success( array() );
+		}
+
+		$restaurant_ids = array_column( $results, 'ID' );
+
+		// Get all of the food from the area restaurants near me.
+		$connected = new WP_Query( array(
+			'connected_type'  => 'food_to_restaurant',
+			'connected_items' => $restaurant_ids,
+			'nopaging'        => true,
+			'posts_per_page'  => 1,
+			'post_status'     => 'publish',
+			'tax_query'       => array(
 				array(
 					'taxonomy' => 'taste',
 					'field'    => 'slug',
 					'terms'    => $taste,
 				),
 			),
-		);
+		) );
 
-		$food = get_posts( $food_query );
+		$food = $connected->have_posts() ? $connected->posts : array();
 
 		wp_send_json_success( $this->parse_food( $food ) );
 	}
@@ -238,6 +339,10 @@ class Food_Scout_API {
 				'slug'        => $result->post_name,
 				'description' => $result->post_content,
 				'food_count'  => 0,
+				'address'     => array(
+					'latitude'  => get_post_meta( $result->ID, 'latitude', true ),
+					'longitude' => get_post_meta( $result->ID, 'longitude', true ),
+				),
 			);
 
 			$objects[] = $object;
